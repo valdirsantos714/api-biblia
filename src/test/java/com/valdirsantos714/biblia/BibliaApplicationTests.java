ac12dcca7de5351000946729ca21c45a45f0f475
Valdir Santos <valdirsantost40@gmail.com>
2026-01-16 20:11:56 -0300

    fix: ajustado BibliaApplicationTests

--- src/test/java/com/valdirsantos714/biblia/BibliaApplicationTests.java
+++ src/test/java/com/valdirsantos714/biblia/BibliaApplicationTests.java
@@ -1,13 +1,14 @@
 package com.valdirsantos714.biblia;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest
+@ExtendWith(MockitoExtension.class)
 class BibliaApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
-
 }
+
